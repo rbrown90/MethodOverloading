de1cd0fc251fc79bd901fc2bdb069d985625e2bc
Rakin Brown <rakinbrown38@gmail.com>
2023-04-15 14:14:46 -0500

    added method overloading

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,33 @@
 {
     public class Program
     {
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2, bool isTrue)
+        {
+            var sum = 0;
+            var response = "";
+
+            if (isTrue)
+            {
+                response = (sum == 1) ? $"{sum} dollars." : $"{sum} dollar.";
+            }
+            return response;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(7, 9));
+            Console.WriteLine(Add(.15m, .20m));
+            Console.WriteLine(Add(5, 6, true));
+
         }
     }
 }
